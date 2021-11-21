aeb07af3e61e7a8819d2bf71deab69ca74915aeb
pfriteau <pfriteau@hotmail.fr>
2021-11-21 21:34:52 +0100

    set lib path

--- cpp/src/faceswap_func/faceswap_func.cpp
+++ cpp/src/faceswap_func/faceswap_func.cpp
@@ -11,3 +11,18 @@ void addition(int a, int b) {
 }
 
 // Define functions here
+
+/*void FindAllHomography(int n_quadrangles, int** Q_coords_src, int ** Q_coords_dst, int ** H){
+    for(int i = 0; i < n_quadrangles; i++){
+        Find_Homography(Q_coords_src[i], Q_coords_dst[i], H[i]);
+    }
+}
+
+int ApplyAllHomography(unsigned char * imgSrc, unsigned char * imgDst, int width, int height, int * imgLabel, int ** H){
+    for(int y = 0; y < height; y++){
+        for(int x = 0; x < width; x++){
+            if (imgLabel[y*width+x] > -1){
+                ApplyPointHomography(imgSrc, imgDst, width, height, H[imgLabel[y*width+x]] );
+            }
+        }
+    }*/
--- testLib.py
+++ testLib.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, "./lib")
+sys.path.insert(0, "./lib/Debug")
 import faceswap_func as fs
 
 fs.hello()
