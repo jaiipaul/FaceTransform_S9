802122c77e76c271cdfec4001de08c72306e1f98
pfriteau <pfriteau@hotmail.fr>
2022-01-21 00:03:37 +0100

    Bigger Face working

--- cpp_cuda/src/biggerface/biggerface.cpp
+++ cpp_cuda/src/biggerface/biggerface.cpp
@@ -29,32 +29,34 @@ void BF_deformation(float* Landmarks_input, float* Landmarks_output){
     double diffy_8_9 = Landmarks_input[17] - Landmarks_input[15];//points 8 et 10
 
     //MODIFICATION X
-    tableau_modifs[6] -= diffx_3_4 / (double)2;//point 4
-    tableau_modifs[26] += diffx_3_4 / (double)2;//point 14
+    double modifX = 1;
+    tableau_modifs[6] -= diffx_3_4 / modifX;//point 4
+    tableau_modifs[26] += diffx_3_4 / modifX;//point 14
 
-    tableau_modifs[8] -= diffx_4_5 / (double)2;//point 5
-    tableau_modifs[24] += diffx_4_5 / (double)2;//point 13
+    tableau_modifs[8] -= diffx_4_5 / modifX;//point 5
+    tableau_modifs[24] += diffx_4_5 / modifX;//point 13
 
-    tableau_modifs[10] -= diffx_5_6 / (double)2;//point 6
-    tableau_modifs[22] += diffx_5_6 / (double)2;//point 12
+    tableau_modifs[10] -= diffx_5_6 / modifX;//point 6
+    tableau_modifs[22] += diffx_5_6 / modifX;//point 12
 
-    tableau_modifs[12] -= diffx_6_7 / (double)2;//point 7
-    tableau_modifs[20] += diffx_6_7 / (double)2;//point 11
+    tableau_modifs[12] -= diffx_6_7 / modifX;//point 7
+    tableau_modifs[20] += diffx_6_7 / modifX;//point 11
 
     //MODIFICATION Y
     //IL FAUT FAIRE LA DIFF AVEC CELUI DU BAS ET PAS CELUI DU HAUT
       //  JE DIVISE PAR 4 POUR PAS TROP MODIFIER, A VOIR CE QUE CA DONNE
-    tableau_modifs[9] += diffy_5_6 / (double)4;//point 5
-    tableau_modifs[25] += diffy_5_6 / (double)4;//point 13
+    double modifY = 1;
+    tableau_modifs[9] += diffy_5_6 / modifY;//point 5
+    tableau_modifs[25] += diffy_5_6 / modifY;//point 13
 
-    tableau_modifs[11] += diffy_6_7 / (double)4;//point 6
-    tableau_modifs[23] += diffy_6_7 / (double)4;//point 12
+    tableau_modifs[11] += diffy_6_7 / modifY;//point 6
+    tableau_modifs[23] += diffy_6_7 / modifY;//point 12
 
-    tableau_modifs[13] += diffy_7_8 / (double)4;//point 7
-    tableau_modifs[21] += diffy_7_8 / (double)4;//point 11
+    tableau_modifs[13] += diffy_7_8 / modifY;//point 7
+    tableau_modifs[21] += diffy_7_8 / modifY;//point 11
 
-    tableau_modifs[15] += diffy_8_9 / (double)4;//point 8
-    tableau_modifs[19] += diffy_8_9 / (double)4;//point 10
+    tableau_modifs[15] += diffy_8_9 / modifY;//point 8
+    tableau_modifs[19] += diffy_8_9 / modifY;//point 10
 
     for (i = 0; i < 136; i++)
     {
@@ -310,7 +312,7 @@ clock_t time;
     //1) Create Labelled image to know wich homographt to apply
     int* imgLabel =NULL;
     imgLabel = (int*)calloc(width_CAM*height_CAM, sizeof(int));
-    BF_CreateLabelledImage(n_quadrangles, Quadrangles_Ptr, landmarksCAM_Ptr, width_CAM, height_CAM, imgLabel);
+    BF_CreateLabelledImage(n_quadrangles, Quadrangles_Ptr, landmarks_deform, width_CAM, height_CAM, imgLabel);
 
     //2) Determine all homographies
     float** H = NULL;
